033be0a33a48e02396d67a796906ddbd7ddd71b9
Jorgenfje92 <147491390+Jorgenfje92@users.noreply.github.com>
2026-03-17 22:21:51 +0100

    Added new URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -10,6 +10,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://citypulse-frontend-production.up.railway.app",
+        "https://citypulse.fjellstadteknologi.no",
     ],
     allow_credentials=True,
     allow_methods=["*"],
